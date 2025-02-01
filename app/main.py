0fe7868c8b0d02322a0262fdf564927346b7ec8d
Simon Boijer <boijersi@arcada.fi>
2025-02-01 23:34:12 +0200

    get all inventory endpoint added, hardcoded db data

--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return { "Hello": "Rahti2", "v": "0.4" }
+#produkt klass så vi följer samma struktur, lägg till/ta bort om jag glömt nåt
+class Product(BaseModel):
+    id: int
+    productCode: str
+    saldo: int
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+#hårdkodad data
+inventory = {
+    1: Product(id=1, productCode="Karhu", saldo=100),
+    2: Product(id=2, productCode="Sandels", saldo=150),
+    3: Product(id=2, productCode="Pepsi max", saldo=7000)
+}
+
+###
+### VÅRA ENDPOINTS
+###
+
+#Returnerar alla items i vårt inventory
+@app.get("/inventory", response_model=list[Product])
+def get_inventory():
+    return list(inventory.values())
