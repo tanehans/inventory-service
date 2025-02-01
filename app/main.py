5359cbbd59f8fa20caa6ae605140d87e8dca75af
Simon Boijer <boijersi@arcada.fi>
2025-02-02 00:05:04 +0200

    Lagt in delete endpoint

--- app/main.py
+++ app/main.py
@@ -1,23 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
-#produkt klass så vi följer samma struktur, lägg till/ta bort om jag glömt nåt
+#Produktklasser
 class Product(BaseModel):
     id: int
     productCode: str
     saldo: int
 
+class ProductDeleteRequest(BaseModel):
+    productCode: str 
 
-#hårdkodad data
+#Hårdkodad data
 inventory = {
     1: Product(id=1, productCode="Karhu", saldo=100),
     2: Product(id=2, productCode="Sandels", saldo=150),
-    3: Product(id=2, productCode="Pepsi max", saldo=7000)
+    3: Product(id=3, productCode="Vatten", saldo=7000)
 }
-
-
 
 ###                 ###
 ### VÅRA ENDPOINTS  ###
@@ -28,10 +28,21 @@ inventory = {
 def get_inventory():
     return list(inventory.values())
 
-#Lägger till en ny produkt till inventory
+#Lägger till en ny produkt
 @app.post("/inventory", response_model=Product, status_code=201)
 def create_product(product: Product):
     if product.id in inventory:
         raise HTTPException(status_code=400, detail="Produkt med samma id finns redan :(")
     inventory[product.id] = product
     return product
+
+#raderar en produkt baserat på produktkoden
+@app.delete("/inventory", status_code=200)
+def delete_product(request: ProductDeleteRequest):
+    product_id = next((key for key, product in inventory.items() if product.productCode == request.productCode), None)
+
+    if product_id is None:
+        raise HTTPException(status_code=404, detail="Produkten finns inte")
+    
+    deleted_product = inventory.pop(product_id)
+    return {"message": f"Produkten {deleted_product.productCode} är borttagen"}
