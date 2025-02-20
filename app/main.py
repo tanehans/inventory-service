0c2a5e0794eea923b0aa48c1563f87f92c700747
Simon Boijer <boijersi@arcada.fi>
2025-02-20 14:58:36 +0200

    updated decrease validation, error if any product balance  is not enough

--- app/main.py
+++ app/main.py
@@ -54,26 +54,23 @@ def delete_multiple_products(request: ProductDeleteMultipleRequest):
 #        öka/sänka saldo
 # =============================
 
-@app.post("/inventory/increase", response_model=Product)
-def increase_stock(request: StockRequest):
-    product_id = check_product_exists(inventory, request.productCode)
-    ensure_valid_quantity(request.quantity)
-
-    inventory[product_id] = inventory[product_id].model_copy(
-        update={"stock": inventory[product_id].stock + request.quantity})
-    return inventory[product_id]
-
 @app.post("/inventory/decrease", response_model=list[Product])
 def decrease_stock(request: DecreaseStockMultipleRequest):
-    updated_products = []
     for item in request.items:
         product_id = check_product_exists(inventory, item.productCode)
         ensure_valid_quantity(item.quantity)
-
+        
         product = inventory[product_id]
         if product.stock < item.quantity:
-            raise HTTPException(status_code=400, detail=f"Inte tillräckligt med lagersaldo för {item.productCode}")
+            raise HTTPException(
+                status_code=400, 
+                detail=f"Inte tillräckligt med lagersaldo för {item.productCode}. Tillgängligt: {product.stock}, Efterfrågat: {item.quantity}"
+            )
 
+    updated_products = []
+    for item in request.items:
+        product_id = check_product_exists(inventory, item.productCode)
+        product = inventory[product_id]
         inventory[product_id] = product.model_copy(update={"stock": product.stock - item.quantity})
         updated_products.append(inventory[product_id])
 
